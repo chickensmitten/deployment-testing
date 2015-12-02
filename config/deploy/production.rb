307ab76b8eb48c3f55758baf519f153da7f127da
chickensmitten <chia.shengyeong@gmail.com>
2015-12-02 12:44:47 +0800

    second commit

---
+++ config/deploy/production.rb
@@ -0,0 +1,26 @@
+set :stage, :production
+set :branch, "master"
+
+set :server_name, "188.166.223.66"
+
+set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+
+server '188.166.223.66', user: 'deploy', roles: %w{web app db}, primary: true
+
+set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
+
+set :rails_env, :production
+
+set :unicorn_worker_count, 5
+
+set :enable_ssl, false
+
+
+
+
+
+
+
+
+
+
