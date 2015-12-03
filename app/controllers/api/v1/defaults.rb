53a449b2db549daa6a5fce6ce45fa1d0bac79874
chickensmitten <chia.shengyeong@gmail.com>
2015-12-03 11:44:41 +0800

    deploying with api

--- app/controllers/api/v1/defaults.rb
+++ app/controllers/api/v1/defaults.rb
@@ -26,9 +26,6 @@ module API
           error_response(message: e.message, status: 404)
         end
 
-        rescue_from ActiveRecord::RecordInvalid do |e|
-          error_response(message: e.message, status: 422)
-        end
       end
 
     end
